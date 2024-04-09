f8c03addb9af838506cb3a77aee62f0d60cfc7ea
Julian Aburrow <jaburrow@btinternet.com>
2024-04-09 10:48:07 +0100

    Moved login logic from MainLayout to App.razor

---
+++ BlazorAuthDemo/App.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorAuthDemo;
+
+public partial class App
+{
+    [Inject] NavigationManager NavigationManager { get; set; } = null!;
+
+    private string ReturnUrl { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        ReturnUrl = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
+    }
+}
--- BlazorAuthDemo/Shared/MainLayout.razor.cs
+++ BlazorAuthDemo/Shared/MainLayout.razor.cs
@@ -2,12 +2,4 @@
 
 public partial class MainLayout
 {
-    [Inject] NavigationManager NavigationManager { get; set; } = null!;
-
-    private string ReturnUrl { get; set; } = default!;
-
-    protected override void OnInitialized()
-    {
-        ReturnUrl = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
-    }
 }
